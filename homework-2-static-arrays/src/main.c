19cc7ca8bd94dfd0bc6e8bc5c0731f55dc7a9fec
Liswiera <vadimboyarkin2000@gmail.com>
2025-11-25 16:47:16 +0700

    Сделать заготовку для ДЗ №2

---
+++ homework-2-static-arrays/src/main.c
@@ -0,0 +1,9 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
---
+++ homework-2-static-arrays/src/test.c
@@ -0,0 +1,15 @@
+#include "../../unity/unity.h"
+#include "lib.h"
+#include <string.h>
+#include <stdio.h>
+
+
+void setUp(void) { }
+
+void tearDown(void) { }
+
+int main(void) {
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
