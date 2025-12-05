f7299bc8fbfcb78310c18f8b892bff887ea75692
Liswiera <vadimboyarkin2000@gmail.com>
2025-12-05 13:33:09 +0700

    Добавить прототипы функций

--- homework-4-structures/src/lib.h
+++ homework-4-structures/src/lib.h
@@ -1,3 +1,26 @@
 #pragma once
 
 #include <inttypes.h>
+
+#define BUF_SIZE 10
+
+struct abonent {
+    char name[BUF_SIZE];
+    char second_name[BUF_SIZE];
+    char tel[BUF_SIZE];
+};
+
+void abonent_print(const struct abonent *obj);
+
+
+struct abonent_vec {
+    struct abonent *abonents;
+    size_t len;
+    size_t capacity;
+};
+
+struct abonent_vec abonent_vec_create(struct abonent *abonents, size_t capacity);
+uint32_t abonent_vec_push(struct abonent_vec *vec, const char *name, const char *second_name, const char *tel);
+uint32_t abonent_vec_remove_at(struct abonent_vec *vec, size_t id);
+void abonent_vec_search_by_name_and_print(const struct abonent_vec *vec, const char *name);
+void abonent_vec_print_all(const struct abonent_vec *vec);
