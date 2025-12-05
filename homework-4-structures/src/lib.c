c57824509c626892abf46c235ba2172349e28652
Liswiera <vadimboyarkin2000@gmail.com>
2025-12-05 15:00:01 +0700

    Добавить реализацию методов в задании №4

--- homework-4-structures/src/lib.c
+++ homework-4-structures/src/lib.c
@@ -1,1 +1,75 @@
 #include "lib.h"
+
+static void strncpy_safe(char *restrict dest, char *restrict src, size_t dest_buf_size) {
+    strncpy(dest, src, dest_buf_size);
+    dest[dest_buf_size - 1] = '\0';
+}
+
+void abonent_create_from(struct abonent *restrict dest, const char *name, const char *second_name, const char *tel) {
+    strncpy_safe(dest->name, name, CHAR_ARR_SIZE);
+    strncpy_safe(dest->second_name, second_name, CHAR_ARR_SIZE);
+    strncpy_safe(dest->tel, tel, CHAR_ARR_SIZE);
+}
+
+void abonent_print(const struct abonent *obj) {
+    printf("Имя: %s; Фамилия: %s; Телефон: %s\n",
+        obj->name,
+        obj->second_name,
+        obj->tel);
+}
+
+
+struct abonent_vec abonent_vec_create(struct abonent *abonents, size_t capacity) {
+    struct abonent_vec vec;
+    vec.abonents = abonents;
+    vec.capacity = capacity;
+    vec.len = 0;
+
+    return vec;
+}
+
+uint32_t abonent_vec_push(struct abonent_vec *restrict vec, const char *name, const char *second_name, const char *tel) {
+    if (vec->len < vec->capacity) {
+        abonent_create_from(&vec->abonents[vec->len], name, second_name, tel);
+        vec->len++;
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+uint32_t abonent_vec_remove_at(struct abonent_vec *vec, size_t id) {
+    if (id < vec->len) {
+        for (size_t pos = id + 1; pos < vec->len; pos++) {
+            struct abonent *dest = &vec->abonents[pos - 1];
+            struct abonent *src = &vec->abonents[pos];
+            memcpy(dest, src, sizeof(struct abonent));
+        }
+        vec->len--;
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+void abonent_vec_search_by_name_and_print(const struct abonent_vec *vec, const char *name) {
+    printf("Поиск абонентов с именем \"%s\":\n", name);
+    for (size_t i = 0; i < vec->len; i++) {
+        if (strcmp(vec->abonents[i].name, name) == 0) {
+            printf("[ID#%zu] ", i);
+            abonent_print(&vec->abonents[i]);
+        }
+    }
+    printf("\n");
+}
+
+void abonent_vec_print_all(const struct abonent_vec *vec) {
+    printf("Вектор абонентов [len = %zu] [capacity = %zu]\n", vec->len, vec->capacity);
+    for (size_t i = 0; i < vec->len; i++) {
+        printf("[ID#%zu] ", i);
+        abonent_print(&vec->abonents[i]);
+    }
+    printf("\n");
+}
--- homework-4-structures/src/lib.h
+++ homework-4-structures/src/lib.h
@@ -1,26 +1,29 @@
 #pragma once
 
 #include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define BUF_SIZE 10
+#define CHAR_ARR_SIZE 10
 
 struct abonent {
-    char name[BUF_SIZE];
-    char second_name[BUF_SIZE];
-    char tel[BUF_SIZE];
+    char name[CHAR_ARR_SIZE];
+    char second_name[CHAR_ARR_SIZE];
+    char tel[CHAR_ARR_SIZE];
 };
 
+void abonent_create_from(struct abonent *restrict dest, const char *name, const char *second_name, const char *tel);
 void abonent_print(const struct abonent *obj);
 
 
 struct abonent_vec {
     struct abonent *abonents;
-    size_t len;
     size_t capacity;
+    size_t len;
 };
 
 struct abonent_vec abonent_vec_create(struct abonent *abonents, size_t capacity);
-uint32_t abonent_vec_push(struct abonent_vec *vec, const char *name, const char *second_name, const char *tel);
+uint32_t abonent_vec_push(struct abonent_vec *restrict vec, const char *name, const char *second_name, const char *tel);
 uint32_t abonent_vec_remove_at(struct abonent_vec *vec, size_t id);
 void abonent_vec_search_by_name_and_print(const struct abonent_vec *vec, const char *name);
 void abonent_vec_print_all(const struct abonent_vec *vec);
