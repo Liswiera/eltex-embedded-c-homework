502b5b9b693f07bfaa2c448f832ad300ea79f42c
Liswiera <vadimboyarkin2000@gmail.com>
2026-02-26 23:03:48 +0700

    Реализовать первый пункт задания №9

--- homework-9-file-system/src/main.c
+++ homework-9-file-system/src/main.c
@@ -5,6 +5,8 @@
 #include "dir_view.h"
 
 #define VIEW_COUNT 2
+#define OUTPUT_STRING "String from file"
+
 
 static void interactive_mode(struct dir_view **views, int view_count) {
     for (int i = 0; i < view_count; i++) {
@@ -104,27 +106,71 @@ static struct dir_view** create_dir_view_subwins(WINDOW *wnd, int n) {
     }
 }
 
-static void init_curses() {
-    initscr();
 
-    start_color();
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    init_pair(2, COLOR_WHITE, COLOR_RED);
-    init_pair(3, COLOR_BLUE, COLOR_BLACK);
-    init_pair(4, COLOR_BLUE, COLOR_RED);
-    init_pair(5, COLOR_CYAN, COLOR_BLACK);
-    init_pair(6, COLOR_CYAN, COLOR_RED);
+static int task_1() {
+    clear();
+    printw("Создание файла test_file.txt...\n");
 
-    cbreak();
-    noecho();
-    curs_set(0);
-}
+    FILE *file = fopen("test_file.txt", "w+");
+    if (file == NULL) {
+        printw("Не удалось создать файл.\n");
+        getch();
+        return 1;
+    }
 
-int task_1() {
+    printw("Запись строки '%s' в файл...\n", OUTPUT_STRING);
+
+    int status = fprintf(file, "%s", OUTPUT_STRING);
+    if (status < 0) {
+        printw("Не удалось записать строку в файл.\n");
+        getch();
+        return 2;
+    }
+
+    printw("Создание буфера...");
+
+    fseek(file, 0L, SEEK_END);
+    long file_size = ftell(file);
+
+    if (file_size < 0) {
+        printw("Не удалось получить размер файла.\n");
+        getch();
+        return 3;
+    }
+
+    char *str_buf = malloc(sizeof(char) * file_size);
+    if (str_buf == NULL) {
+        printw("Не удалось создать буфер.\n");
+        getch();
+        return 4;
+    }
+
+    printw("Считывание строки из файла...\n");
+    
+    for (long i = 0; i < file_size; i++) {
+        fseek(file, -(i + 1), SEEK_END);
+        size_t byte_count = fread(str_buf + i, 1, 1, file);
+
+        if (byte_count != 1) {
+            printw("Не удалось считать строку из файла.\n");
+            free(str_buf);
+            getch();
+            return 5;
+        }
+    }
+
+    printw("Полученная строка: ");
+    addnstr(str_buf, file_size);
+    printw("\n");
+    
+    free(str_buf);
+    getch();
     return 0;
 }
 
-int task_2() {
+static int task_2() {
+    clear();
+
     struct dir_view **views = create_dir_view_subwins(stdscr, VIEW_COUNT);
     if (views == NULL) {
         endwin();
@@ -142,6 +188,22 @@ int task_2() {
     return 0;
 }
 
+static void init_curses() {
+    initscr();
+
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_WHITE, COLOR_RED);
+    init_pair(3, COLOR_BLUE, COLOR_BLACK);
+    init_pair(4, COLOR_BLUE, COLOR_RED);
+    init_pair(5, COLOR_CYAN, COLOR_BLACK);
+    init_pair(6, COLOR_CYAN, COLOR_RED);
+
+    cbreak();
+    noecho();
+    curs_set(0);
+}
+
 int main() {
     setlocale(LC_ALL, "ru_RU.UTF-8");
     init_curses();
@@ -151,8 +213,11 @@ int main() {
     printw("2. Файловый менеджер\n");
 
     int result = 0;
+    int done = FALSE;
 
-    do {
+    while (!done) {
+        done = TRUE;
+
         int ch = getch();
         switch (ch) {
             case '1':
@@ -162,9 +227,9 @@ int main() {
                 result = task_2();
                 break;
             default:
-                continue;
+                done = FALSE;
         }
-    } while(FALSE);
+    }
 
     endwin();
     return result;
